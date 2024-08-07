c0f7c379b56cf3cd1d224f649e862315ed95343d
IAMPARVp2 <95540208+IAMPARVp2@users.noreply.github.com>
2024-08-07 17:39:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -9,8 +9,23 @@ public:
         return ans;
 
     }
+
+    int topDownSolve(int n, vector<int>dp){
+         if( n==0 || n==1 ){
+            return n;
+        };
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n] = topDownSolve(n-1,dp)+ topDownSolve(n-2,dp);
+        return dp[n];
+
+
+    }
     int fib(int n) {
-        int ans = recSolve(n);
+        vector<int> dp(n+1,-1);
+
+        int ans = topDownSolve(n, dp);
         return ans;
     }
 };
