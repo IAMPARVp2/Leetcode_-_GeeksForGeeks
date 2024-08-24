0a377e8ccfa806a364deed7eb6e3c0744d07079f
IAMPARVp2 <95540208+IAMPARVp2@users.noreply.github.com>
2024-08-25 02:17:49 +0530

    Time: 16 ms (10.96%), Space: 16.9 MB (52.34%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,87 +1,29 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-       //space = O(m + n)
-    // int n = matrix.size();
-    // int m = matrix[0].size();
-    // vector<int> row(n, 0);
-    // vector<int> col(m, 0);
+        int m = matrix.size();    // Number of rows
+        int n = matrix[0].size(); // Number of columns
+        
+        vector<int> row(m, 0);    // Array to mark rows with zero
+        vector<int> col(n, 0);    // Array to mark columns with zero
 
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = 0; j < m; j++)
-    //     {
-    //         if (matrix[i][j] == 0)
-    //         {
-    //             row[i] = 1;
-    //             col[j] = 1;
-    //         }
-    //     }
-    // }
-
-    // for (int i = 0; i < row.size(); i++)
-    // {
-    //     for (int j = 0; j < m; j++)
-    //     {
-    //         if (row[i] or col[j])
-    //         {
-    //             matrix[i][j] = 0;
-    //         }
-    //     }
-    // }
-
-    // Space  = O(1)
-
-    int n = matrix.size();
-    int m = matrix[0].size();
-    int col0 = 1;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (matrix[i][j] == 0)
-            {
-
-                matrix[i][0] = 0;
-
-                if (j != 0)
-                    matrix[0][j] = 0;
-                else
-                    col0 = 0;
+        // First pass to find which rows and columns need to be set to zero
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
             }
         }
-    }
 
-    for (int i = 1; i < n; i++)
-    {
-        for (int j = 1; j < m; j++)
-        {
-            if (matrix[i][j] != 0)
-            {
-
-                if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                {
+        // Second pass to set the rows and columns to zero
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] == 1 || col[j] == 1) {
                     matrix[i][j] = 0;
                 }
             }
         }
     }
-
-    if (matrix[0][0] == 0)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            matrix[0][j] = 0;
-        }
-    }
-    if (col0 == 0)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            matrix[i][0] = 0;
-        }
-    } 
-}
-
 };
