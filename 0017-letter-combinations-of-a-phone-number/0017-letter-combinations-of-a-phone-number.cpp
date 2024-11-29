517561af1b8f7ede1089fa9ce0c433d8c1d4a1ec
IAMPARVp2 <95540208+IAMPARVp2@users.noreply.github.com>
2024-11-30 02:43:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        string output="";
+        if(digits.length()==0){
+            return output;
+        }
+        vector<int>ans;
+        int index =0;
+        vector <string >mapping(10);
+        mapping[2]="abc";
+        mapping[2]="abc";
+        mapping[2]="abc";
+        mapping[2]="abc";
+        mapping[2]="abc";mapping[2]="abc";
+
+    }
+};
