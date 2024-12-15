3896df5b276a889442ed3b18028e4a0645b6715c
IAMPARVp2 <95540208+IAMPARVp2@users.noreply.github.com>
2024-12-15 21:51:59 +0530

    Time: 0 ms (100%), Space: 26 MB (27.56%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& nums, int start, int end){
+    int binarySearch(vector<int>& arr, int start, int end){
+        while(start<=end){
 
         int mid = start + (end - start)/2;
-        if(nums.size()==1){return nums[0];}
-        if(nums[0]!=nums[1]){return nums[0];}
-        if(nums[nums.size()-1]!=nums[nums.size()-2]){return nums[nums.size()-1];}
-        if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
-            return nums[mid];
+        if(start == end){return arr[start];}
+            if(mid % 2 == 0){
+                if(arr[mid]==arr[mid+1]){
+                start=mid+2;
+                }
+                else{end=mid;}  
+            }
+            else{
+                if(arr[mid]==arr[mid-1]){
+                start=mid+1;
+                }
+                else{end=mid-1;}
+            }
         }
-        binarySearch(nums, start, mid-1);
-        binarySearch(nums, mid+1,end);
+        
+      
 
         return -1;
     }
 
 
     int singleNonDuplicate(vector<int>& nums) {
-        int start =1;
-        int end =nums.size()-2;
+        int start =0;
+        int end =nums.size()-1;
         
         int ans = binarySearch(nums, start, end);
         return ans;
